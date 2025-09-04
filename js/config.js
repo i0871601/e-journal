1faf28981c72968fc3974fb6a1c89538b3c1cd4a
i0871601 <i0871601@gmail.com>
2025-09-04 20:06:48 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
 // Авторське право (c) серпень 2025 рік Сікан Іван Валерійович.
 
 export const API_URL = "https://worker-home.i0871601.workers.dev/";
+export const API_URL_SCHEDULE = "https://worker-schedule.i0871601.workers.dev/";
