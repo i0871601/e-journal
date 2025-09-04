b9c8951d888ab42e33d6a8ca020fcbe764864ec7
i0871601 <i0871601@gmail.com>
2025-09-04 22:52:16 +0300

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -2,6 +2,8 @@
 
 export const API_URL_AUTHORIZATION = "https://worker-home.i0871601.workers.dev/";
 export const API_URL_SCHEDULE = "https://worker-schedule.i0871601.workers.dev/";
-
-
-
+export const API_URL_UPDATE_GRADE = "https://worker-update-grade.i0871601.workers.dev/";
+export const API_URL_FULL_JOURNAL = "https://worker-full-journal.i0871601.workers.dev/";
+export const API_URL_ADD_LESSON = "https://worker-add-lesson.i0871601.workers.dev/";
+export const API_URL_GRADES_JOURNAL = "https://worker-grades-of-journal.i0871601.workers.dev/";
+export const API_URL_STUDENT_JOURNAL = "https://worker-student-full-journal.i0871601.workers.dev/";
