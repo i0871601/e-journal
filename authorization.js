bb374f4d562f3235b65c02bedad0b7c2790da40d
i0871601 <i0871601@gmail.com>
2025-09-04 21:07:17 +0300

    Update authorization.js

--- authorization.js
+++ authorization.js
@@ -1,6 +1,6 @@
 // Авторське право (c) серпень 2025 рік Сікан Іван Валерійович.
-import { setButtonState, displayErrorMessage, clearErrorMessage } from './js/dom.js';
-import { hashPassword, authorizeUser, updatePassword } from './js/api.js';
+import { setButtonState, displayErrorMessage, clearErrorMessage } from './js/auth/dom.js';
+import { hashPassword, authorizeUser, updatePassword } from './js/auth/api.js';
 
 const form = document.getElementById('loginForm');
 const passwordField = document.getElementById('password');
@@ -124,5 +124,6 @@ export function initAuth() {
 }
 
 document.addEventListener('DOMContentLoaded', initAuth);
+
 
 
