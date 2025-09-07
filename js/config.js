eabec1e5c2d33eb6b7fd7290e13bbaff5331d226
i0871601 <i0871601@gmail.com>
2025-09-07 14:03:23 +0300

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Авторське право (c) серпень 2025 рік Сікан Іван Валерійович.
-export const API_URL_AUTHORIZATION = "https://worker-home.i3an.workers.dev/";
+export const API_URL_AUTHORIZATION = "https://worker-refuge.i3an.workers.dev/";
 export const API_URL_SCHEDULE = "https://worker-schedule.i3an.workers.dev/";
 export const API_URL_UPDATE_GRADE = "https://worker-update-grade.i0871601.workers.dev/";
 export const API_URL_FULL_JOURNAL = "https://worker-full-journal.i0871601.workers.dev/";
