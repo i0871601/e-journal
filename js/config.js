5fda82e41913c74140b5118ba5699c821ba7327a
i0871601 <i0871601@gmail.com>
2025-09-11 17:33:45 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,10 +2,10 @@
 export const API_URL_AUTHORIZATION = "https://worker-refuge.i087.workers.dev/";
 export const API_URL_SCHEDULE = "https://worker-refuge.i087.workers.dev/";
 export const API_URL_UPDATE_GRADE = "https://worker-update-grade.i0871601.workers.dev/";
-export const API_URL_FULL_JOURNAL = "https://worker-full-journal.i0871601.workers.dev/";
+export const API_URL_FULL_JOURNAL = "https://worker-refuge.i087.workers.dev/";
 export const API_URL_ADD_LESSON = "https://worker-add-lesson.i0871601.workers.dev/";
 export const API_URL_GRADES_JOURNAL = "https://worker-class-subject.i0871601.workers.dev/";
-export const API_URL_STUDENT_JOURNAL = "https://worker-student-full-journal.i0871601.workers.dev/";
+export const API_URL_STUDENT_JOURNAL = "https://worker-refuge.i087.workers.dev/";
 
 export function getUserData() {
     try {
@@ -52,6 +52,7 @@ export async function request(url, payload) {
         return { success: false, message: error.message };
     }
 }
+
 
 
 
