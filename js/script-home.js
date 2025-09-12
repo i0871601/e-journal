52e2971275f25a2854625e014e3504060f5d71a5
i0871601 <i0871601@gmail.com>
2025-09-12 18:34:31 +0300

    Update and rename script-home.js to js/script-home.js

--- js/script-home.js
+++ js/script-home.js
@@ -1,18 +1,18 @@
 // Авторське право (c) серпень 2025 рік Сікан Іван Валерійович.
-//if (sessionStorage.length === 0) {
-//    //window.location.href = 'index.html';
-//}
-//window.addEventListener('pageshow', (event) => {
-//    //if (event.persisted) {
-//    //    sessionStorage.clear();
-//    //    const form = document.getElementById('loginForm');
-//    //    form.reset();
-//    //}
-//});
+if (sessionStorage.length === 0) {
+    window.location.href = '../index.html';
+}
+window.addEventListener('pageshow', (event) => {
+    if (event.persisted) {
+        sessionStorage.clear();
+        const form = document.getElementById('loginForm');
+        form.reset();
+    }
+});
 
-import { getUserData } from './js/config.js';
-import { initScheduleLogic } from './js/schedule.js';
-import { initDropdown } from './js/dropdown-handler.js';
+import { getUserData } from './config.js';
+import { initScheduleLogic } from './schedule.js';
+import { initDropdown } from './dropdown-handler.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const allCheckboxes = document.querySelectorAll('.toggle-checkbox');
@@ -40,6 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
 
 
 
