1056b63f93d8270211b7748eb9fe3a1b3482be09
i0871601 <i0871601@gmail.com>
2025-09-26 14:45:26 +0300

    Обнова

--- js/schedule.js
+++ js/schedule.js
@@ -15,7 +15,7 @@ const daysList = document.getElementById('DaysList');
 const selectedTextContainer = document.getElementById('first-option');
 const checkbox = document.getElementById('days-checkbox');
 
-function TimeNow (startTime, endTime, checkTime, nextStartTime, entryArticle, statusIcon){
+function TimeNow (startTime, endTime, checkTime, maxTime, entryArticle, statusIcon){
     if(!checkTime){ return;}
     const now = new Date();
     const currentHours = now.getHours();
@@ -30,24 +30,27 @@ function TimeNow (startTime, endTime, checkTime, nextStartTime, entryArticle, st
     if (currentTotalMinutes >= startTotalMinutes && currentTotalMinutes < endTotalMinutes){
         entryArticle.classList.add('current');
         statusIcon.classList.add('current');
-    } else { //якщо урок не поточний перевіряє чи є наступний урок
-        if (nextStartTime) {//знайдений початок наступного уроку
-            const [nextStartHours, nextStartMinutes] = nextStartTime.split(':').map(Number);
-            const nextTotalMinutes = nextStartHours * 60 + nextStartMinutes;
-            //перевіряє чи час між уроками тобто перерва і урок пройшов
-            if (currentTotalMinutes >= endTotalMinutes && currentTotalMinutes < nextTotalMinutes){
-                entryArticle.classList.add('passed');
-                statusIcon.classList.add('passed');
-            } else if(currentTotalMinutes >= endTotalMinutes){//перевіряє для інших уроків чи вони закінчились
-                entryArticle.classList.add('passed');
-                statusIcon.classList.add('passed');
+    } else {
+        if (maxTime) {//знайдений кінець всіх уроків
+            const [maxHours, maxMinutes] = maxTime.split(':').map(Number);
+            const maxTotalMinutes = maxHours * 60 + maxMinutes;
+            if (maxTotalMinutes == endTotalMinutes){
+                if (currentTotalMinutes >= startTotalMinutes && currentTotalMinutes < endTotalMinutes +30){
+                    entryArticle.classList.add('end');
+                } 
             }
-        } else if (nextStartTime == null){//якщо не знайшовся урок початок наступного значить він один або останій
-            //перевіряє чи урок пройшов та відображує його ще 30 хвилин
-            if (currentTotalMinutes > startTotalMinutes && currentTotalMinutes <= endTotalMinutes + 30){
-                entryArticle.classList.add('passed');
-                statusIcon.classList.add('passed');
-            }
+        }
+    }
+}
+
+function markPassed(){
+    const current = document.querySelector('.schedule-entry.current, .schedule-entry.end');
+    if(current){
+        let prevElement = current.previousElementSibling;
+        while(prevElement){
+            prevElement.classList.add('passed');
+            prevElement.querySelector('.status-icon').classList.add('passed');
+            prevElement = prevElement.previousElementSibling;
         }
     }
 }
@@ -116,9 +119,8 @@ export const displaySchedule = (groupedByDay, role, selectedDay) => {
             entryArticle.appendChild(infoBlock);
             contentSchedule.appendChild(entryArticle);
 
-            const nextStartTime = (index + 1 < dayData.length) ? dayData[index + 1].Time.split('-')[0] : null;
-            TimeNow (startTime, endTime, checkTime, nextStartTime, entryArticle, statusIcon);
         });
+        TimeNow(checkTime);
     } else {
         contentSchedule.textContent = 'На цей день розклад відсутній.';
     }
