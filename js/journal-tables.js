261a6b7d0616dd7dec50feeb66011b479f9b25e1
i0871601 <i0871601@gmail.com>
2026-05-01 09:47:25 +0300

    Update journal-tables.js

--- js/journal-tables.js
+++ js/journal-tables.js
@@ -1,5 +1,7 @@
 // Авторське право (c) серпень 2025 рік Сікан Іван Валерійович.
 export function displayGrades(journalData, name) {
+    const tablejournalOp = document.querySelector(".TabletJournal");
+    tablejournalOp.style.opacity = '0';
     const tableContainer = document.querySelector(".journal-content");
     if (!tableContainer) {
         console.error("Елемент з класом '.journal-content' не знайдено.");
@@ -13,6 +15,7 @@ export function displayGrades(journalData, name) {
 
     if ((!lessons || lessons.length === 0) || (!grades || grades.length === 0)) {
         tableContainer.innerHTML = '<p>Журнал пустий. Немає записів та оцінок в журналі.</p>';
+        tablejournalOp.style.opacity = '1';
         return;
     }
     const table = document.createElement('table');
@@ -82,6 +85,7 @@ export function displayGrades(journalData, name) {
     
     table.appendChild(tableBody);
     tableContainer.appendChild(table);
+    tablejournalOp.style.opacity = '1';
 
     document.querySelectorAll('thead th').forEach(headerCell => {
         if (headerCell.dataset.lessonNumber) {
@@ -103,6 +107,8 @@ export function displayGrades(journalData, name) {
     });
 }
 export function displayFullJournal(journalData, updateGradeCallback) {
+    const tablejournalOp = document.querySelector(".TabletJournal");
+    tablejournalOp.style.opacity = '0';
     const tableContainer = document.querySelector(".journal-content");
     if (!tableContainer) {
         console.error("Елемент з класом '.journal-content' не знайдено.");
@@ -117,6 +123,7 @@ export function displayFullJournal(journalData, updateGradeCallback) {
 
     if (!students || students.length === 0) {
         tableContainer.innerHTML = '<p>Журнал пустий. Немає учнів в класі.</p>';
+        tablejournalOp.style.opacity = '1';
         return;
     }
 
@@ -210,6 +217,7 @@ export function displayFullJournal(journalData, updateGradeCallback) {
 
     table.appendChild(tableBody);
     tableContainer.appendChild(table);
+    tablejournalOp.style.opacity = '1';
 
     document.querySelectorAll('.grade-input').forEach(input => {
         input.addEventListener('change', (event) => {
