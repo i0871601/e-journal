01206a8e7cdf3b6c4bc4dfba6cb8268c977487f3
i0871601 <i0871601@gmail.com>
2026-05-16 20:21:44 +0300

    0.00.045

--- script.js
+++ script.js
@@ -25,6 +25,29 @@ export const createListDay = (routine) => {
     return uniqueDays.sort((a, b) => dayOrder.indexOf(a) - dayOrder.indexOf(b));
 };
 
+export const renderDaysList = (listDay) => {
+    //Знаходимо список ul на сторінці
+    const daysListContainer = document.getElementById('DaysList');
+    
+    if (!daysListContainer) {
+        console.error("Елемент <ul id='DaysList'> не знайдено на сторінці!");
+        return;
+    }
+    daysListContainer.innerHTML = '';
+    // Перевіряємо, чи масив днів не порожній
+    if (!Array.isArray(listDay) || listDay.length === 0) {
+        daysListContainer.innerHTML = '<li>Пусто</li>';
+        return;
+    }
+    //Перебирає кожен день і створюємо для нього <li>
+    listDay.forEach(day => {
+        const li = document.createElement('li');
+        li.textContent = day;
+
+        daysListContainer.appendChild(li);
+    });
+};
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -38,4 +61,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const listDay = createListDay(routine); 
     console.log("Унікальні дні тижня:", listDay);
+
+    renderDaysList(listDay);
 });
