97aad26c760b639096bea266a2b5b923581b2054
i0871601 <i0871601@gmail.com>
2026-05-19 18:54:51 +0300

    0.00.091

---
+++ script-journal.js
@@ -0,0 +1,24 @@
+// Авторське право (c) травень 2026 рік Сікан Іван Валерійович.
+import { request, getUserData } from './js/config.js';
+
+if (sessionStorage.length === 0) {
+    window.location.href = '../index.html';
+}
+window.addEventListener('pageshow', (event) => {
+    if (event.persisted) {
+        sessionStorage.clear();
+        const form = document.getElementById('loginForm');
+        form.reset();
+    }
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const userData = getUserData();
+
+    let test = [];
+    if (userData && userData.data.classes) {
+        test = userData.data.classes;
+        console.log("Ось ваш масив:", test);
+    }
+});
